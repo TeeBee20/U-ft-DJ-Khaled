a7552a64179453529460a413a8bda3c2ed918204
Khizar Iqbal <khizar.i0066@gmail.com>
2021-08-28 11:58:46 +0100

    spotify auth and player added

--- you-ft-djkhaled/src/components/song.js
+++ you-ft-djkhaled/src/components/song.js
@@ -1,42 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Album from "./album";
 
 const Song = (props) => {
-  const [trackList, setTrackList] = useState([]);
-  const { djName, song } = props;
+	const [trackList, setTrackList] = useState([]);
+	const { djName, song } = props;
 
-  const updateAlbum = () => {
-    setTrackList((currTrackList) => {
-      if (currTrackList.includes(song)) {
-        alert("You just played yo self - and this song too");
-        return currTrackList;
-      }
-      const newList = [...currTrackList, song];
-      return newList;
-    });
-  };
+	const updateAlbum = () => {
+		setTrackList((currTrackList) => {
+			if (currTrackList.includes(song)) {
+				alert("You just played yo self - and this song too");
+				return currTrackList;
+			}
+			const newList = [...currTrackList, song];
+			return newList;
+		});
+	};
 
-  const deleteTrack = () => {
-    setTrackList((currTrackList) => {
-      const copy = [...currTrackList];
-      copy.pop();
-      return copy;
-    });
-  };
+	const deleteTrack = () => {
+		setTrackList((currTrackList) => {
+			const copy = [...currTrackList];
+			copy.pop();
+			return copy;
+		});
+	};
 
-  return (
-    <div className="track">
-      <h3>Your New Track</h3>
-      <p className="ftdjkhaled">
-        <span>{song}</span> - {djName} ft. DJ KHALED
-      </p>
-      <Album
-        trackList={trackList}
-        updateAlbum={updateAlbum}
-        deleteTrack={deleteTrack}
-      />
-    </div>
-  );
+	return (
+		<div className="track">
+			<h3>Your New Track</h3>
+			<p className="ftdjkhaled">
+				<span>{song}</span> - {djName} ft. DJ KHALED
+			</p>
+
+			<Album
+				trackList={trackList}
+				updateAlbum={updateAlbum}
+				deleteTrack={deleteTrack}
+			/>
+		</div>
+	);
 };
 
 export default Song;
--- you-ft-djkhaled/src/components/your_name.js
+++ you-ft-djkhaled/src/components/your_name.js
@@ -1,26 +1,26 @@
 const YourName = (props) => {
-  const { djName, setdjName } = props;
+	const { djName, setdjName } = props;
 
-  return (
-    <div>
-      <h3 className="enterName">Sup playa?! Enter a fire DJ name 🎧</h3>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <label htmlFor="djName"></label>
-        <input
-          id="djName"
-          placeholder="🔥 DJ Name Here Dawg 🔥"
-          value={djName}
-          onChange={(event) => {
-            setdjName(event.target.value);
-          }}
-        ></input>
-      </form>
-    </div>
-  );
+	return (
+		<div>
+			<h3 className="enterName">Sup playa?! Enter a fire DJ name 🎧</h3>
+			<form
+				onSubmit={(event) => {
+					event.preventDefault();
+				}}
+			>
+				<label htmlFor="djName"></label>
+				<input
+					id="djName"
+					placeholder="🔥 DJ Name Here Dawg 🔥"
+					value={djName}
+					onChange={(event) => {
+						setdjName(event.target.value);
+					}}
+				></input>
+			</form>
+		</div>
+	);
 };
 
 export default YourName;
