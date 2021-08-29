7ca477b6a2d42e99f66143f8bd93a10e7a49add9
Khizar Iqbal <khizar.i0066@gmail.com>
2021-08-29 19:26:48 +0100

    redirect URI fixed in Login

--- you-ft-djkhaled/src/components/Login.js
+++ you-ft-djkhaled/src/components/Login.js
@@ -1,6 +1,6 @@
 import { Container } from "react-bootstrap";
 const AUTH_URL =
-	"https://accounts.spotify.com/authorize?client_id=5a55cf15e70f4f6b9e20972e1c37b58c&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-read-playback-state%20user-modify-playback-state";
+	"https://accounts.spotify.com/authorize?client_id=5a55cf15e70f4f6b9e20972e1c37b58c&response_type=code&redirect_uri=https://u-ft-djkhaled.netlify.app&scope=streaming%20user-read-email%20user-read-private%20user-read-playback-state%20user-modify-playback-state";
 export default function Login() {
 	return (
 		<Container>
